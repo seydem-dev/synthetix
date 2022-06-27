b8945d5766058a3c499bfa2e3feb7e720c66119f
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-06-27 21:20:12 +0200

    Initialized

---
+++ contracts/Weth.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WETH is ERC20 {
+    
+    constructor() ERC20("Wrapped Ether", "WETH") {}
+
+    event Deposit(address indexed from, uint256 amount);
+    event Withdraw(address indexed to, uint256 amount);
+
+    function deposit() public payable {
+        _mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amount) external {
+        _burn(msg.sender, amount);
+        payable(msg.sender).transfer(amount);
+        emit Withdraw(msg.sender, amount);
+    }
+
+    fallback() external payable {
+        deposit();
+    }
+}
